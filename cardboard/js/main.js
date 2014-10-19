e09597b0c5cfa4fbf0339bcc2db3b7dcaffe05d8
Siena McFetridge <scmcfetridge>
2014-10-18 23:04:26 -0500

    Actually adding to the correct file

--- cardboard/js/main.js
+++ cardboard/js/main.js
@@ -121,45 +121,70 @@ renderer.setClearColorHex( 0xa3a3a3, 1 );
 }
 
 Leap.loop( function( frame ) {
-    var hand, direction, len;
-    if ( frame.hands.length > 0) {
-      hand = frame.hands[0];
-      palm.position.set( hand.stabilizedPalmPosition[0], hand.stabilizedPalmPosition[1], hand.stabilizedPalmPosition[2] );
-      direction = new THREE.Vector3(hand.direction[0], hand.direction[1], hand.direction[2] );  // best so far
-      palm.lookAt( direction.add( palm.position ) );
-      palm.rotation.z = -hand.roll();
-      //palm.rotation.set( hand.pitch(), -hand.yaw(), hand.roll() );
-      palm.visible = true;
-
-      data.innerHTML = 'Hand X:' + hand.stabilizedPalmPosition[0].toFixed(0) + ' Y:' +  hand.stabilizedPalmPosition[1].toFixed(0) + ' Z:' + hand.stabilizedPalmPosition[2].toFixed(0);
-
-    } else {
-      palm.visible = false;
+    if (frame.valid && frame.gestures.length > 0) {
+        frame.gestures.forEach(function(gesture) {
+            if(gesture.type == "swipe") {
+                // Erase screen    
+                var swipeDirection;
+                //Classify swipe as either horizontal or vertical
+              var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+              //Classify as right-left or up-down
+              if(isHorizontal){
+                  if(gesture.direction[0] > 0){
+                      swipeDirection = "right";
+                  } else {
+                      swipeDirection = "left";
+                  }
+              } else { //vertical
+                  if(gesture.direction[1] > 0){
+                      swipeDirection = "up";
+                  } else {
+                      swipeDirection = "down";
+                  }                  
+              }
+              console.log(swipeDirection)
+            }
+        });
     }
-
-    len = frame.pointables.length;
-    if ( len > 0) {
-      var pointable;
-      palm.hasFingers = true;
-      for (var i = 0; i < 5; i++) {
-        finger = fingers[i];
-        if ( i < len ) {
-          pointable = frame.pointables[i];
-          finger.position.set( pointable.stabilizedTipPosition[0], pointable.stabilizedTipPosition[1], pointable.stabilizedTipPosition[2] );
-          direction = v( pointable.direction[0], pointable.direction[1], pointable.direction[2]);
-          finger.lookAt( direction.add( finger.position ) );
-          finger.scale.z = pointable.length;
-          finger.visible = true;
-        } else {
-          finger.visible = false;
-        }
-      }
-    } else if ( palm.hasFingers ) {
-      for (var i = 0; i < 5; i++) {
-        fingers[i].visible = false;
-      }
-      palm.hasFingers = false;
-    }
+//    var hand, direction, len;
+//    if ( frame.hands.length > 0) {
+//      hand = frame.hands[0];
+//      palm.position.set( hand.stabilizedPalmPosition[0], hand.stabilizedPalmPosition[1], hand.stabilizedPalmPosition[2] );
+//      direction = new THREE.Vector3(hand.direction[0], hand.direction[1], hand.direction[2] );  // best so far
+//      palm.lookAt( direction.add( palm.position ) );
+//      palm.rotation.z = -hand.roll();
+//      //palm.rotation.set( hand.pitch(), -hand.yaw(), hand.roll() );
+//      palm.visible = true;
+//
+//      data.innerHTML = 'Hand X:' + hand.stabilizedPalmPosition[0].toFixed(0) + ' Y:' +  hand.stabilizedPalmPosition[1].toFixed(0) + ' Z:' + hand.stabilizedPalmPosition[2].toFixed(0);
+//
+//    } else {
+//      palm.visible = false;
+//    }
+//
+//    len = frame.pointables.length;
+//    if ( len > 0) {
+//      var pointable;
+//      palm.hasFingers = true;
+//      for (var i = 0; i < 5; i++) {
+//        finger = fingers[i];
+//        if ( i < len ) {
+//          pointable = frame.pointables[i];
+//          finger.position.set( pointable.stabilizedTipPosition[0], pointable.stabilizedTipPosition[1], pointable.stabilizedTipPosition[2] );
+//          direction = v( pointable.direction[0], pointable.direction[1], pointable.direction[2]);
+//          finger.lookAt( direction.add( finger.position ) );
+//          finger.scale.z = pointable.length;
+//          finger.visible = true;
+//        } else {
+//          finger.visible = false;
+//        }
+//      }
+//    } else if ( palm.hasFingers ) {
+//      for (var i = 0; i < 5; i++) {
+//        fingers[i].visible = false;
+//      }
+//      palm.hasFingers = false;
+//    }
   });
 
 function resize() {
