6e0c10013ab684495d30e7c2e5a12de1babba6a3
Paige Hinkle <paigehinkle@utexas.edu>
2014-10-24 19:45:35 -0500

    fog tinker

--- cardboard/js/main.js
+++ cardboard/js/main.js
@@ -126,7 +126,7 @@ function init() {
     scene.add(ball);
   }
 
-  scene.fog = new THREE.FogExp2(0xA8CBD8, .00001);
+  scene.fog = new THREE.FogExp2(0xA8CBD8, .0025);
 
   // var point1 = [60, 10, 90];
   // var point2 = [20, 30, 70];
